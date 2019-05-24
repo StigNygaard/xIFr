1d19eae2026f37f8ef747c8a1137a3f80295b9a0
Stig Nygaard <StigNygaard@users.noreply.github.com>
2019-05-24 12:28:39 +0200

    xmp fix - handle if null at end

--- xmp.js
+++ xmp.js
@@ -35,8 +35,12 @@ function xmpClass()
     
     let utf8decoder = new TextDecoder('utf-8');
     let xmlString = utf8decoder.decode(xml);
-    var dom = parser.parseFromString(xmlString, 'application/xml');
     //console.debug("xmp xmlString: \n" + xmlString);
+    if (xmlString.charCodeAt(xmlString.length-1) == 0) {
+      xmlString = xmlString.substring(0,xmlString.length-1);
+      //console.debug("xmp xmlString modified: \n" + xmlString);
+    }
+
 
     if (dom.documentElement.nodeName == 'parsererror') {
       // parsererror might have been caused by incorrect encoding of characters.
