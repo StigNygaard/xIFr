03bd3499b9b77a0ea89fc50075d2e23e0fb9d2ac
Stig Nygaard <stig@rockland.dk>
2020-12-13 16:59:35 +0100

    Upboarding

--- WebExtension/backgroundscript.js
+++ WebExtension/backgroundscript.js
@@ -168,7 +168,7 @@ function handleInstalled({ reason, temporary, previousVersion }) {
       browser.tabs.create({ url: "onboard/onboard.html"});
       break;
     case "update":
-      // No upboarding in this version...
+      browser.tabs.create({ url: "onboard/onboard.html"});
       break;
   }
 }
