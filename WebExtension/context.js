8ce6ade5238bbda4f200abb698fbebf9eb7b0183
Stig Nygaard <stig@rockland.dk>
2021-04-07 20:46:37 +0200

    Add versionnumber object

--- WebExtension/context.js
+++ WebExtension/context.js
@@ -78,3 +78,68 @@ var context = (function Context() {
   };
 
 })();
+
+var versionnumber = (function Versionnumber() {  // major.minor.revision
+  function current() {
+    if (browser && browser.runtime) { // webextension versionnumber
+      return browser.runtime.getManifest().version;
+    }
+    // undefined
+  }
+  function compare(v1, v2) {
+    if (typeof v2 === 'undefined') {
+      v2 = v1;
+      v1 = current();
+    }
+    let v1parts = v1.split('.');
+    let v2parts = v2.split('.');
+    while (v1parts.length < v2parts.length) v1parts.push(0);
+    while (v2parts.length < v1parts.length) v2parts.push(0);
+    for (let i = 0; i < v1parts.length; ++i) {
+      if (parseInt(v1parts[i], 10) > parseInt(v2parts[i], 10)) {
+        return 1;
+      } else if (parseInt(v1parts[i], 10) < parseInt(v2parts[i], 10)) {
+        return -1;
+      }
+    }
+    return 0;
+  }
+  function parts(v, n) {
+    if (typeof n === 'undefined') {
+      n = v;
+      v = current();
+    }
+    let vparts = v.split('.', n);
+    vparts.map( function(item) {return String(parseInt(item.trim(),10))});
+    while (vparts.length < n) vparts.push('0');
+    return vparts.join('.');
+  }
+  function major(v) {
+    if (typeof v === 'undefined') {
+      v = current();
+    }
+    return parts(v, 1);
+  }
+  function minor(v) {
+    if (typeof v === 'undefined') {
+      v = current();
+    }
+    return parts(v, 2);
+  }
+  function revision(v) {
+    if (typeof v === 'undefined') {
+      v = current();
+    }
+    return parts(v, 3);
+  }
+
+  // API:
+  return {
+    current,
+    compare,
+    major,
+    minor,
+    revision
+  };
+
+})();
