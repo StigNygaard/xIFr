461fd9c73aa2550f8bce750af477c7bfcde18bba
Stig Nygaard <stig@rockland.dk>
2024-03-24 11:49:55 +0100

    fix Options opening twice

--- WebExtension/boarding/boarding.js
+++ WebExtension/boarding/boarding.js
@@ -8,7 +8,10 @@ function init() {
     elm.textContent = currentVersion;
   });
   document.querySelectorAll('.settings').forEach((elm) => {
-    elm.addEventListener('click', () => browser.runtime.openOptionsPage())
+    elm.addEventListener('click', (ev) => {
+      ev.preventDefault();
+      browser.runtime.openOptionsPage();
+    })
   });
   let vboarding = currentVersion;
   if (previousVersion) vboarding += (',' + previousVersion);
