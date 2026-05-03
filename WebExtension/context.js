e8d51c2098de37268bae9e5f55c6598d93540b70
Stig Nygaard <stig@rockland.dk>
2026-05-03 21:27:34 +0200

    code style

--- WebExtension/context.js
+++ WebExtension/context.js
@@ -9,19 +9,19 @@ globalThis.context = globalThis.context || (function Context() {
   const INFO = false; // false
   const LOG = false; // false
   function log(...arg) {
-    if (console && LOG) console.log(...arg);
+    if (LOG && console) console.log(...arg);
   }
   function info(...arg) {
-    if (console && INFO) console.info(...arg);
+    if (INFO && console) console.info(...arg);
   }
   function debug(...arg) {
-    if (console && DEBUG) console.debug(...arg);
+    if (DEBUG && console) console.debug(...arg);
   }
   function warn(...arg) {
-    if (console && WARN) console.warn(...arg);
+    if (WARN && console) console.warn(...arg);
   }
   function error(...arg) {
-    if (console && ERROR) console.error(...arg);
+    if (ERROR && console) console.error(...arg);
   }
 
   // Options:
@@ -66,7 +66,7 @@ globalThis.context = globalThis.context || (function Context() {
     return !!(typeof browser !== 'undefined' && browser?.menus?.getTargetElement); // Well, might not be enough. But for the time being this check should tell. In practice Firefox 63+ supports, Chrome does not...
   }
   function prefersDark(dispMode) {
-    return dispMode === "darkMode" || window?.matchMedia('(prefers-color-scheme: dark)')?.matches;
+    return dispMode === "darkMode" || globalThis?.matchMedia('(prefers-color-scheme: dark)')?.matches;
   }
   function isFirefox() {
     return !!((typeof browser !== 'undefined') && browser?.runtime?.getURL("./")?.startsWith("moz-extension://"));
@@ -118,40 +118,40 @@ globalThis.versionnumber = globalThis.versionnumber || (function Versionnumber()
     while (v1parts.length < v2parts.length) v1parts.push(0);
     while (v2parts.length < v1parts.length) v2parts.push(0);
     for (let i = 0; i < v1parts.length; ++i) {
-      if (parseInt(v1parts[i], 10) > parseInt(v2parts[i], 10)) {
+      if (Number.parseInt(v1parts[i], 10) > Number.parseInt(v2parts[i], 10)) {
         return 1;
-      } else if (parseInt(v1parts[i], 10) < parseInt(v2parts[i], 10)) {
+      } else if (Number.parseInt(v1parts[i], 10) < Number.parseInt(v2parts[i], 10)) {
         return -1;
       }
     }
     return 0;
   }
   function parts(v, n) {
-    if (typeof n === 'undefined') {
+    if (n === undefined) {
       n = v;
       v = current();
     }
     const vparts = v.split('.', n);
     vparts.map(function (item) {
-      return String(parseInt(item.trim(), 10))
+      return String(Number.parseInt(item.trim(), 10))
     });
     while (vparts.length < n) vparts.push('0');
     return vparts.join('.');
   }
   function major(v) {
-    if (typeof v === 'undefined') {
+    if (v === undefined) {
       v = current();
     }
     return parts(v, 1);
   }
   function minor(v) {
-    if (typeof v === 'undefined') {
+    if (v === undefined) {
       v = current();
     }
     return parts(v, 2);
   }
   function revision(v) {
-    if (typeof v === 'undefined') {
+    if (v === undefined) {
       v = current();
     }
     return parts(v, 3);
@@ -161,8 +161,8 @@ globalThis.versionnumber = globalThis.versionnumber || (function Versionnumber()
       const vparts = v.split('.');
       if (vparts.length >= 1 && vparts.length <= 3) {
         for (const part of vparts) {
-          const parsed = parseInt(part, 10);
-          if (isNaN(parsed)) return false;
+          const parsed = Number.parseInt(part, 10);
+          if (Number.isNaN(parsed)) return false;
           if (part !== String(parsed)) return false; // This doesn't allow leading 0s like in yyyy.mm.dd versionnumbers (ok?)
         }
       }
